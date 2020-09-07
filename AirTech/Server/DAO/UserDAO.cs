34fb303b70301df0e21140222d1bc377022b7c6e
melodiely <mly@intechinfo.fr>
2020-09-07 16:02:44 +0200

    Add all the objects models

---
+++ AirTech/Server/DAO/UserDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using AirTech.Shared;
+using AirTech.Server.Interface;
+
+namespace AirTech.Server.DAO
+{
+    internal class UserDAO
+    {
+        IDatabaseService _databaseService;
+        
+        public UserDAO(IDatabaseService databaseService)
+        {
+            this._databaseService = databaseService;
+        }
+
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User CreateUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AirTech/Shared/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AirTech.Shared
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+    }
+}
